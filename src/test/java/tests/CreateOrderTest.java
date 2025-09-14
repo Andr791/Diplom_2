87a16bfe4962ffd60deb10548fca3ad8ff6ba463
Klinikov Andr <azxqwe1@mail.ru>
2025-09-15 01:29:27 +0300

    создание тестов

--- src/test/java/tests/CreateOrderTest.java
+++ src/test/java/tests/CreateOrderTest.java
@@ -6,13 +6,12 @@ import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
 import static steps.Steps.*;
 
 public class CreateOrderTest extends BaseClass {
 
     @Test
-    @DisplayName("Создать заказ")
+    @DisplayName("Создать заказ - позитивный тест")
     @Description("С авторизацией, с ингредиентами")
     public void createOrderPozivive() {
         String email = "azxqwe1@mail.ru";
@@ -33,33 +32,62 @@ public class CreateOrderTest extends BaseClass {
         deleteUser(accessToken);
     }
 
-  /*  @Test
-    @DisplayName("Получить список заказов")
-    @Description("Проверка, что в тело ответа возвращается список заказов")
-    public void listOfOrders() {
-        Response response = listOrders();
-        response.then().assertThat().body("orders", notNullValue())
+    @Test
+    @DisplayName("Создать заказ - негативный тест")
+    @Description("с авторизацией, без ингредиентов")
+    public void createOrderNegativeNoIngredients() {
+        String email = "azxqwe1@mail.ru";
+        String password = "10458617";
+        String name = "Andrey";
+        Response response = userCreate(email, password, name);
+        String accessToken = response.then().extract().body().path("accessToken");
+
+        String[] ingredients = new String[]{};
+        Response newOrder = createOrder(ingredients, accessToken);
+        newOrder.then().assertThat().body("message", equalTo("Ingredient ids must be provided"))
                 .and()
-                .statusCode(200);
+                .statusCode(400);
+
+        deleteUser(accessToken);
     }
 
     @Test
-    @DisplayName("Получить список заказов")
-    @Description("Проверка, что в тело ответа возвращается список заказов")
-    public void listOfOrders() {
-        Response response = listOrders();
-        response.then().assertThat().body("orders", notNullValue())
-                .and()
-                .statusCode(200);
+    @DisplayName("Создать заказ - негативный тест")
+    @Description("с авторизацией, с неверным хешем ингредиентов")
+    public void createOrderNegativeInvalidId() {
+        String email = "azxqwe1@mail.ru";
+        String password = "10458617";
+        String name = "Andrey";
+        Response response = userCreate(email, password, name);
+        String accessToken = response.then().extract().body().path("accessToken");
+
+        String[] ingredients = new String[]{"60d3b"};
+        Response newOrder = createOrder(ingredients, accessToken);
+        newOrder.then()
+                .statusCode(500);
+
+        deleteUser(accessToken);
     }
 
     @Test
-    @DisplayName("Получить список заказов")
-    @Description("Проверка, что в тело ответа возвращается список заказов")
-    public void listOfOrders() {
-        Response response = listOrders();
-        response.then().assertThat().body("orders", notNullValue())
+    @DisplayName("Создать заказ без авторизации")
+    @Description("Токен не передаем в запросе")
+    public void createOrderNoAuthorization() {
+        String email = "azxqwe1@mail.ru";
+        String password = "10458617";
+        String name = "Andrey";
+        Response response = userCreate(email, password, name);
+        String accessToken = response.then().extract().body().path("accessToken");
+
+        Response responseIngredients = ingredients();
+        String ingredientsOne = responseIngredients.then().extract().body().path("data[0]._id");
+        String ingredientsTwo = responseIngredients.then().extract().body().path("data[1]._id");
+        String[] ingredients = new String[]{ingredientsOne, ingredientsTwo};
+        Response newOrder = createOrder(ingredients, "");
+        newOrder.then().assertThat().body("success", equalTo(true)) // в документации и по факту без авторизации заказ через API создается!
                 .and()
                 .statusCode(200);
-    }*/
+
+        deleteUser(accessToken);
+    }
 }
--- src/test/java/tests/ListOfOrdersTest.java
+++ src/test/java/tests/ListOfOrdersTest.java
@@ -5,17 +5,45 @@ import io.restassured.response.Response;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
+import static steps.Steps.*;
+import static steps.Steps.deleteUser;
 
 public class ListOfOrdersTest extends BaseClass {
 
-  /*  @Test
-    @DisplayName("Получить список заказов")
+    @Test
+    @DisplayName("Получить список заказов пользователя")
     @Description("Проверка, что в тело ответа возвращается список заказов")
     public void listOfOrders() {
-        Response response = listOrders();
-        response.then().assertThat().body("orders", notNullValue())
+        String email = "azxqwe1@mail.ru";
+        String password = "10458617";
+        String name = "Andrey";
+        Response response = userCreate(email, password, name);
+        String accessToken = response.then().extract().body().path("accessToken");
+
+        Response responseIngredients = ingredients();
+        String ingredientsOne = responseIngredients.then().extract().body().path("data[0]._id");
+        String ingredientsTwo = responseIngredients.then().extract().body().path("data[1]._id");
+        String[] ingredients = new String[]{ingredientsOne, ingredientsTwo};
+        createOrder(ingredients, accessToken);
+
+        Response responseOrders = orders(accessToken);
+        responseOrders.then().assertThat().body("orders[0]._id", notNullValue())
                 .and()
                 .statusCode(200);
-    }*/
+
+        deleteUser(accessToken);
+    }
+
+    @Test
+    @DisplayName("Получить список заказов пользователя без авторизации")
+    @Description("Проверка ошибки")
+    public void listOfOrdersNegative() {
+        Response responseOrders = orders("");
+        responseOrders.then().assertThat().body("message", equalTo("You should be authorised"))
+                .and()
+                .statusCode(401);
+    }
+
 }
